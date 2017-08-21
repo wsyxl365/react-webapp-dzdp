2c17afccaec24ffd7953275e99398045117e3684
yexiaolong <771388996@qq.com>
2017-08-21 16:32:45 +0800

    edit ad href

--- mock/home/ad.js
+++ mock/home/ad.js
@@ -2,31 +2,31 @@ module.exports = [
     {
         title: '暑假5折',
         img: 'http://www.gosolo.top/blog/shujia.png',
-        link: 'http://www.imooc.com/wap/index'
+        link: 'http://www.github.com'
     },
     {
         title: '特价出国',
         img: 'http://www.gosolo.top/blog/chuguo.png',
-        link: 'http://www.imooc.com/wap/index'
+        link: 'http://www.github.com'
     },
     {
         title: '亮亮车',
         img: 'http://www.gosolo.top/blog/xiche.png',
-        link: 'http://www.imooc.com/wap/index'
+        link: 'http://www.github.com'
     },
     {
         title: '学钢琴',
         img: 'http://www.gosolo.top/blog/gangqing.png',
-        link: 'http://www.imooc.com/wap/index'
+        link: 'http://www.github.com'
     },
     {
         title: '电影',
         img: 'http://www.gosolo.top/blog/dianying.png',
-        link: 'http://www.imooc.com/wap/index'
+        link: 'http://www.github.com'
     },
     {
         title: '旅游热线',
         img: 'http://www.gosolo.top/blog/lvyou.png',
-        link: 'http://www.imooc.com/wap/index'
+        link: 'http://www.github.com'
     }
 ]
